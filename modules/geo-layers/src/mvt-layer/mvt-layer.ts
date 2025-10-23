194998bbef6ad241f5cad71053628c015821cd79
s-n <nikita.sharakhov@AXLE-MacBookPro-C02CW3F1MD6R.local>
2025-10-22 20:57:17 -0400

    Move code

--- modules/geo-layers/src/mvt-layer/mvt-layer.ts
+++ modules/geo-layers/src/mvt-layer/mvt-layer.ts
@@ -254,21 +254,20 @@ export default class MVTLayer<
       tile: Tile2DHeader<ParsedMvtTile>;
     }
   ): Layer | null | LayersList {
-    const {x, y, z} = props.tile.index;
-    const worldScale = Math.pow(2, z);
-
-    const xScale = WORLD_SIZE / worldScale;
-    const yScale = -xScale;
-
-    const xOffset = (WORLD_SIZE * x) / worldScale;
-    const yOffset = WORLD_SIZE * (1 - y / worldScale);
-
-    const modelMatrix = new Matrix4().scale([xScale, yScale, 1]);
-
     props.autoHighlight = false;
 
     if (!this.context.viewport.resolution) {
       if (this.context.viewport.isGeospatial) {
+        const {x, y, z} = props.tile.index;
+        const worldScale = Math.pow(2, z);
+
+        const xScale = WORLD_SIZE / worldScale;
+        const yScale = -xScale;
+
+        const xOffset = (WORLD_SIZE * x) / worldScale;
+        const yOffset = WORLD_SIZE * (1 - y / worldScale);
+
+        const modelMatrix = new Matrix4().scale([xScale, yScale, 1]);
         props.modelMatrix = modelMatrix;
         props.coordinateOrigin = [xOffset, yOffset, 0];
       } else {
